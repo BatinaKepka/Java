d6be39f0e4d07cb9f3caf6ad63e67125c2c2bca8
BatinaKepka <viktor.shevchenko2000@mail.ru>
2023-05-23 18:03:27 +0700

    Using array of objects

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Main
 {
 	
 	
 	public static void main(String[] args)
 	{
-		Line line1 = new Line();
+		/*Line line1 = new Line();
 		Line line2 = new Line();	
 		line1.Input();
 		line2.Input();
@@ -17,5 +19,22 @@ public class Main
 		System.out.printf("Now there are %d objects of class Line\n", Line.getCount());
 		line3.finalize();
 		System.out.printf("Now there are %d objects of class Line\n", Line.getCount());
+		*/
+		int size;
+		System.out.print("Enter size of array: ");
+		Scanner scanner = new Scanner(System.in);      	
+		size = scanner.nextInt();
+		Point array_points[] = new Point[size];
+		System.out.print("Enter array of points:\n ");
+		for(int i = 0; i <size; i++)
+		{
+			array_points[i] = new Point();
+			array_points[i].Input();
+		}
+		System.out.print("Array of points: ");
+		for(int i = 0; i <size; i++)
+		{
+			array_points[i].Display();
+		}
     }
 }
